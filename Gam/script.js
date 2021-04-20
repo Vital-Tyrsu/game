646d419778225558974bf80d3e15338921940e0a
abdelini <55691048+abdelini@users.noreply.github.com>
2021-04-20 18:06:33 +0200

    modal created

--- Gam/script.js
+++ Gam/script.js
@@ -1,21 +1,30 @@
+let button = document.getElementById('btnSubmit');
+button.addEventListener('click', function () {
+    let inpCode = document.getElementById('password');
 
-(function () {
-    'use strict';
-
-    let button = document.getElementById('btnSubmit');
-    button.addEventListener('click', function () {
-        let inpCode = document.getElementById('password');
-
-        if (inpCode.value === "9999") {
-            console.log(2);
-            window.location.replace("communication.html");
-        } else {
-            console.log("ERROR")
-        }
-    });
-
+    if (inpCode.value === "9999") {
+        console.log(2);
+        window.location.replace("communication.html");
+    } else {
+        console.log("ERROR")
+    }
 });
+// Get the modal
+var modal = document.getElementById("myModal");
+let i = 0;
+let speed = 0.1;
+let tekst = document.getElementById("realTekst");
+    
+typeWriter();
 
+function typeWriter() {
+    if (i < tekst.innerHTML.length) {
+        document.getElementById("tekstContainer").innerHTML += tekst.innerHTML.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }else {
+        modal.style.display = "block";
+    }
 
-// set up text to print, each item in array is new line
+}
 
